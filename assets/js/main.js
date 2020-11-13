b4359b28fadcc72dc631227317ac8407e5ce09e4
Alfredo <alfredogregorio.lozano@gmail.com>
2020-11-13 17:28:50 +0100

    esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,11 +14,14 @@ var totalKm = Number(userDistance * 0.21);
 //sconto minorenni
 if (userAge < 18) {
   var scontoMinorenni = Number ((totalKm * 20) / 100);
-  document.getElementById('minor').innerHTML= totalKm - scontoMinorenni;
+  document.getElementById('minor').innerHTML= Math.round(Number(totalKm - scontoMinorenni));
+  document.getElementById('discount').innerHTML= 'Prezzo scontato del 20% perchè sei un pischello'
 }
+//sconto anziani
 if (userAge > 65) {
   var anziani = Number((totalKm * 40) / 100);
-  document.getElementById('older').innerHTML= totalKm - anziani;
+  document.getElementById('older').innerHTML= Math.round(Number(totalKm - anziani));
+  document.getElementById('discount').innerHTML= 'Prezzo scontato del 40% per i matusa sopra i 65'
 }
 
 
@@ -33,11 +36,12 @@ if (isNaN(userDistance)) {
 } else {
   document.getElementById('user_km').innerHTML= userDistance;
 }
-//inserimento dati
+//inserimento dati in html
 document.getElementById('user_name').innerHTML=userName;
 document.getElementById('user_age').innerHTML=userAge;
 document.getElementById('total_price').innerHTML=totalKm;
-// prove console.log
-console.log(totalKm);
-console.log(isNaN(userDistance));
-console.log(scontoMinorenni);
+
+// // prove console.log
+// console.log(totalKm);
+// console.log(isNaN(userDistance));
+// console.log(scontoMinorenni);
